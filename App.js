8af79400846c6fe0f638ecb3e960de5acee07608
KomalSanhotra <84337867+KomalSanhotra@users.noreply.github.com>
2022-03-18 16:29:57 -0400

    final project

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import './App.css';
+import TodoList from './src/components/TodoList';
+function App() {
+  return (
+    <div class='todo-app'>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
